849723f7019956fd83ff24c75a902c4eae64e6d2
cezar <cezar@NOTEB>
2021-12-25 01:57:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioCondicional1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+                Console.WriteLine("Negativo");
+            else
+            Console.WriteLine("positivo");
+
+        }
+    }
+}
